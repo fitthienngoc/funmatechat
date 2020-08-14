b6c1301f2b5ad65d935b0ed01abb6667e75088aa
Thien Ngoc <ngocnt@funtap.vn>
2020-08-14 17:50:22 +0700

    ss

--- api/webhook.js
+++ api/webhook.js
@@ -22,32 +22,32 @@ module.exports = (req, res) => {
     res.status(200).send(req.query['hub.challenge'])
   }
 
-  // if (req && req.body && req.entry) {
-  //   let { body } = req
-  //   let { entry } = body
-  //   if (entry && entry.length > 0) {
-  //     for (let index = 0; index < entry.length; index++) {
-  //       let element = entry[index];
-  //       let mess = element.messaging
-  //       if (mess && mess.length > 0) {
-  //         for (let i_ = 0; i_ < mess.length; i_++) {
-  //           let element2 = mess[i_];
-  //           sendTextMessage(element2.sender ? element2.sender.id : false, element2.message ? element2.message.text : false)
-  //           // sendTextMessage(element2.sender ? element2.sender.id : false, element2.message ? element2.message.text : false)
-  //           // if (element2.message && element2.message.nlp) {
-  //           //   let { id } = element2.sender
-  //           //   let { text } = element2.message
-  //           //   console.log(id, text)
-  //           //   let sendZ = sendTextMessage(id, "Tui là bot đây: " + text)
-  //           // } else {
-  //           //   res.status(200).send("not send")
-  //           // }
-  //         }
-  //       }
-  //     }
-  //   }
-  //   res.status(200).send('ok')
-  // }
+  if (req && req.body && req.entry) {
+    let { body } = req
+    let { entry } = body
+    if (entry && entry.length > 0) {
+      for (let index = 0; index < entry.length; index++) {
+        let element = entry[index];
+        let mess = element.messaging
+        if (mess && mess.length > 0) {
+          for (let i_ = 0; i_ < mess.length; i_++) {
+            let element2 = mess[i_];
+            sendTextMessage(element2.sender ? element2.sender.id : false, element2.message ? element2.message.text : false)
+            // sendTextMessage(element2.sender ? element2.sender.id : false, element2.message ? element2.message.text : false)
+            // if (element2.message && element2.message.nlp) {
+            //   let { id } = element2.sender
+            //   let { text } = element2.message
+            //   console.log(id, text)
+            //   let sendZ = sendTextMessage(id, "Tui là bot đây: " + text)
+            // } else {
+            //   res.status(200).send("not send")
+            // }
+          }
+        }
+      }
+    }
+    res.status(200)
+  }
 }
 
 
